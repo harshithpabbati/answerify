c363c6ec5bbfc27f9b61cfd2cf88161f7c68cc04
harshithpabbati <pabbatiharshith@gmail.com>
2024-04-15 19:07:56 +0530

    setup initial page

---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+const description =
+  'Stop ticket trouble! Answerify uses the power of AI to automatically answer your customer questions, saving you time and delighting your customers.';
+
+export const siteConfig = {
+  name: 'Answerify: Your AI-Powered Support Ticket Superhero',
+  url: 'https://answerify.app',
+  description,
+  hero: {
+    heading: 'Your AI-Powered Support Ticket Superhero',
+    description,
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
