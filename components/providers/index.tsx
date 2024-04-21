c6f9212fdc3ebe2e6a77920fca8a2e4ebee1da82
harshithpabbati <pabbatiharshith@gmail.com>
2024-04-21 18:32:32 +0530

    add openpanel for tracking

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { getUser } from '@/actions/auth';
+import { OpenpanelProvider } from '@openpanel/nextjs';
 
 import { JotaiProvider } from '@/components/providers/JotaiProvider';
 
-export function Providers({ children }: React.PropsWithChildren<{}>) {
-  return <JotaiProvider>{children}</JotaiProvider>;
+export async function Providers({ children }: React.PropsWithChildren<{}>) {
+  const {
+    data: { user },
+  } = await getUser();
+  return (
+    <JotaiProvider>
+      <OpenpanelProvider
+        url="https://api.openpanel.dev"
+        clientId="71e50127-4c7d-4624-987c-db31a909d538"
+        trackScreenViews={true}
+        trackAttributes={true}
+        trackOutgoingLinks={true}
+        profileId={user?.id}
+      />
+      {children}
+    </JotaiProvider>
+  );
 }
