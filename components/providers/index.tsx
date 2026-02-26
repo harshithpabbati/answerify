4d1f1d8287464a5fa587545f3176c15bd1540031
Harshith Pabbati <pabbatiharshith@gmail.com>
2026-02-25 20:40:09 -0500

    Remove openpanel (#10)

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { getUser } from '@/actions/auth';
-import { OpenPanelComponent } from '@openpanel/nextjs';
 
 import { JotaiProvider } from '@/components/providers/JotaiProvider';
 
@@ -8,17 +7,5 @@ export async function Providers({ children }: React.PropsWithChildren<{}>) {
   const {
     data: { user },
   } = await getUser();
-  return (
-    <JotaiProvider>
-      <OpenPanelComponent
-        apiUrl="https://api.openpanel.dev"
-        clientId="71e50127-4c7d-4624-987c-db31a909d538"
-        trackScreenViews={true}
-        trackAttributes={true}
-        trackOutgoingLinks={true}
-        profileId={user?.id}
-      />
-      {children}
-    </JotaiProvider>
-  );
+  return <JotaiProvider>{children}</JotaiProvider>;
 }
